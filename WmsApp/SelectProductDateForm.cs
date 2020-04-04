8b975e92f71a5ada71699ea7fba34edaafa43065
524300045@qq.com <524300045@qq.com>
2020-04-04 23:18:07 +0800

    提交

---
+++ WmsApp/SelectProductDateForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WmsApp
+{
+    public partial class SelectProductDateForm : Form
+    {
+
+        public string ProductDateStr = "";
+        public SelectProductDateForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSure_Click(object sender, EventArgs e)
+        {
+            ProductDateStr = dtDate.Value.ToString("yyyy-MM-dd");
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
+    }
+}
