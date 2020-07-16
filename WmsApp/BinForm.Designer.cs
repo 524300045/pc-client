5f527813981861b55f5006fd890bd1ad83340ec9
524300045@qq.com <524300045@qq.com>
2020-07-16 15:29:22 +0800

    11

---
+++ WmsApp/BinForm.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace WmsApp
+{
+    partial class BinForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pageSplit1 = new Wms.Controls.Pager.PageSplit();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.cbArea = new System.Windows.Forms.ComboBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chk = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.areaCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.locationCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.locationName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Controls.Add(this.pageSplit1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 72);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1065, 429);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.chk,
+            this.areaCode,
+            this.locationCode,
+            this.locationName});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 17);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(1059, 375);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.RowPostPaint += new System.Windows.Forms.DataGridViewRowPostPaintEventHandler(this.dataGridView1_RowPostPaint);
+            // 
+            // pageSplit1
+            // 
+            this.pageSplit1.BackColor = System.Drawing.Color.LightGray;
+            this.pageSplit1.Description = "";
+            this.pageSplit1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pageSplit1.Location = new System.Drawing.Point(3, 392);
+            this.pageSplit1.Name = "pageSplit1";
+            this.pageSplit1.PageCount = 1;
+            this.pageSplit1.PageNo = 1;
+            this.pageSplit1.Size = new System.Drawing.Size(1059, 34);
+            this.pageSplit1.TabIndex = 0;
+            this.pageSplit1.PageChanged += new System.EventHandler(this.pageSplit1_PageChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnExport);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btnQuery);
+            this.groupBox1.Controls.Add(this.cbArea);
+            this.groupBox1.Controls.Add(this.tbName);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1065, 72);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "查询条件";
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(671, 33);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 5;
+            this.btnExport.Text = "导出";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(566, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "打印";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Location = new System.Drawing.Point(453, 29);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(75, 23);
+            this.btnQuery.TabIndex = 3;
+            this.btnQuery.Text = "查询";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // cbArea
+            // 
+            this.cbArea.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbArea.FormattingEnabled = true;
+            this.cbArea.Location = new System.Drawing.Point(74, 31);
+            this.cbArea.Name = "cbArea";
+            this.cbArea.Size = new System.Drawing.Size(85, 20);
+            this.cbArea.TabIndex = 2;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(244, 31);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(158, 21);
+            this.tbName.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(193, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "库位:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "库区:";
+            // 
+            // chk
+            // 
+            this.chk.HeaderText = "选择";
+            this.chk.Name = "chk";
+            // 
+            // areaCode
+            // 
+            this.areaCode.DataPropertyName = "areaCode";
+            this.areaCode.HeaderText = "库区";
+            this.areaCode.Name = "areaCode";
+            this.areaCode.ReadOnly = true;
+            // 
+            // locationCode
+            // 
+            this.locationCode.DataPropertyName = "locationCode";
+            this.locationCode.HeaderText = "货位编码";
+            this.locationCode.Name = "locationCode";
+            this.locationCode.ReadOnly = true;
+            // 
+            // locationName
+            // 
+            this.locationName.DataPropertyName = "locationName";
+            this.locationName.HeaderText = "货位名称";
+            this.locationName.Name = "locationName";
+            this.locationName.ReadOnly = true;
+            // 
+            // BinForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1065, 501);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "BinForm";
+            this.Text = "货位管理";
+            this.Load += new System.EventHandler(this.GoodsBarCodePrintForm_Load);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Wms.Controls.Pager.PageSplit pageSplit1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.ComboBox cbArea;
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn chk;
+        private System.Windows.Forms.DataGridViewTextBoxColumn areaCode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn locationCode;
+        private System.Windows.Forms.DataGridViewTextBoxColumn locationName;
+    }
+}
---
+++ WmsApp/BinForm.cs
@@ -0,0 +1,439 @@
+﻿using Sdbs.Wms.Controls.Pager;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Wms.Controls.Pager;
+using WmsSDK;
+using WmsSDK.Model;
+using WmsSDK.Request;
+using WmsSDK.Response;
+using ZXing;
+using ZXing.Common;
+using ZXing.QrCode;
+
+namespace WmsApp
+{
+    public partial class BinForm : TabWindow
+    {
+
+        private IWMSClient client = null;
+
+        private PaginatorDTO paginator;
+
+        private SortableBindingList<WmsSDK.Model.LocationsModel> sortList = null;
+
+
+        public BinForm()
+        {
+            InitializeComponent();
+            client = new DefalutWMSClient();
+
+        }
+
+
+
+        private void btnQuery_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                paginator = new PaginatorDTO { PageNo = 1, PageSize = 30 };
+                btnQuery.Enabled = false;
+                BindDgv();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("查询异常");
+            }
+            finally
+            {
+                btnQuery.Enabled = true;
+            }
+        }
+
+     
+
+     
+
+        private void BindDgv()
+        {
+
+
+            string areaCode = cbArea.SelectedValue.ToString();
+
+
+            LocationsQueryRequest request = new LocationsQueryRequest();
+            request.warehouseCode = UserInfo.WareHouseCode;
+            request.areaCode = areaCode;
+            request.locationName = tbName.Text.Trim();
+            request.page = paginator.PageNo;
+
+            LocationsResponse response = client.Execute(request);
+
+            if (!response.IsError)
+            {
+
+                if (response.result == null)
+                {
+
+                    this.dataGridView1.DataSource = null;
+                    MessageBox.Show("没有打印任务!");
+                    return;
+                }
+
+
+
+                int recordCount = response.pageUtil.totalRow;
+                int totalPage;
+                if (recordCount % paginator.PageSize == 0)
+                {
+                    totalPage = recordCount / paginator.PageSize;
+                }
+                else
+                {
+                    totalPage = recordCount / paginator.PageSize + 1;
+                }
+
+                IPagedList<LocationsModel> pageList = new PagedList<LocationsModel>(response.result, recordCount, totalPage);
+                sortList = new SortableBindingList<LocationsModel>(pageList.ContentList);
+                this.dataGridView1.DataSource = sortList;
+                pageSplit1.Description = "共查询到" + pageList.RecordCount + "条记录";
+                pageSplit1.PageCount = pageList.PageCount;
+                pageSplit1.PageNo = paginator.PageNo;
+                pageSplit1.DataBind();
+            }
+        }
+
+        private void GoodsBarCodePrintForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                this.dataGridView1.AutoGenerateColumns = false;
+                DatagridViewCheckBoxHeaderCell cbHeader = new DatagridViewCheckBoxHeaderCell();
+
+                cbHeader.Value = string.Empty;
+
+                cbHeader.OnCheckBoxClicked += new CheckBoxClickedHandler(cbHeader_OnCheckBoxClicked);
+                dataGridView1.Columns[0].HeaderCell = cbHeader;
+
+                paginator = new PaginatorDTO { PageNo = 1, PageSize = 30 };
+
+
+
+                List<WarehouseArea> list = new List<WarehouseArea>();
+                list.Add(new WarehouseArea() { areaCode = "A", areaName = "A" });
+                list.Add(new WarehouseArea() { areaCode = "B", areaName = "B" });
+                list.Add(new WarehouseArea() { areaCode = "AB", areaName = "AB" });
+                list.Insert(0, new WarehouseArea() { areaCode = "", areaName = "全部" });
+                cbArea.DataSource = list;
+                cbArea.DisplayMember = "areaName";
+                cbArea.ValueMember = "areaCode";
+
+                BindDgv();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("加载异常" + ex.Message);
+            }
+
+        }
+
+        private void cbHeader_OnCheckBoxClicked(bool state)
+        {
+            //这一句很重要结束编辑状态
+            dataGridView1.EndEdit();
+            if (dataGridView1.Rows.Count > 0)
+            {
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    dataGridView1.Rows[i].Cells[0].Value = state;
+                }
+            }
+        }
+
+
+
+
+
+
+        private void pageSplit1_PageChanged(object sender, EventArgs e)
+        {
+            paginator.PageNo = pageSplit1.PageNo;
+            BindDgv();
+        }
+
+
+        private void pd_PrintBoxPage(object sender, PrintPageEventArgs e) //触发打印事件
+        {
+       
+                Bitmap bt = CreateQRCode(curLocationCode);
+                GetPrintPicture(bt, e);
+          
+
+        }
+
+        public static Bitmap CreateQRCode(string asset)
+        {
+            EncodingOptions options = new QrCodeEncodingOptions
+            {
+                DisableECI = true,
+                CharacterSet = "UTF-8",
+                Width = 320,
+                Height = 320
+            };
+            BarcodeWriter writer = new BarcodeWriter();
+            writer.Format = BarcodeFormat.QR_CODE;
+            writer.Options = options;
+            return writer.Write(asset);
+        }
+
+  
+
+        public void GetPrintPicture(Bitmap image, PrintPageEventArgs g)
+        {
+            int x = 10;
+            int y = 100;
+            int imageY = 10;
+
+            String[] barCodeArr = curLocationCode.Split('-');
+
+            string bar1 = barCodeArr[0];
+            string bar2 = barCodeArr[1];
+            string bar3 = barCodeArr[2];
+
+            Font fontCu = new Font("宋体", 30f, FontStyle.Bold);
+            Brush brush = new SolidBrush(Color.Black);
+            g.Graphics.SmoothingMode = SmoothingMode.HighQuality;
+
+            RectangleF layoutRectangle = new RectangleF(x, y, 100f, 120f);
+            g.Graphics.DrawString(bar1, fontCu, brush, layoutRectangle);
+
+            y += 60;
+            layoutRectangle = new RectangleF(x, y, 100f, 120f);
+            g.Graphics.DrawString(bar2, fontCu, brush, layoutRectangle);
+
+            y += 60;
+            layoutRectangle = new RectangleF(x, y, 100f, 120f);
+            g.Graphics.DrawString(bar3, fontCu, brush, layoutRectangle);
+
+
+            //左上角二维码
+            Rectangle dest2Rect = new Rectangle(x + 80, imageY, image.Width, image.Height);
+            g.Graphics.DrawImage(image, dest2Rect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel);
+
+
+            layoutRectangle = new RectangleF(x + 150, imageY + image.Height - 30, 300f, 120f);
+            g.Graphics.DrawString(curLocationCode, new Font("宋体", 22f, FontStyle.Bold), brush, layoutRectangle);
+
+            //绘制线
+            //Pen Pen = new Pen(Brushes.Black, 1);
+            //Point p1 = new Point(0, 520);
+            //Point p2 = new Point(1400, 520);
+            //g.Graphics.DrawLine(Pen, p1, p2);
+
+
+            //Point p3 = new Point(700, 0);
+            //Point p4 = new Point(700, 1400);
+            //g.Graphics.DrawLine(Pen, p3, p4);
+
+
+        }
+
+        private void dataGridView1_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            var grid = sender as DataGridView;
+            var rowIdx = (e.RowIndex + 1).ToString();
+            var centerFormat = new StringFormat()
+            {
+                Alignment = StringAlignment.Center,
+                LineAlignment = StringAlignment.Center
+            };
+            var headerBounds = new Rectangle(e.RowBounds.Left, e.RowBounds.Top, grid.RowHeadersWidth, e.RowBounds.Height);
+            e.Graphics.DrawString(rowIdx, this.Font, SystemBrushes.ControlText, headerBounds, centerFormat);
+        }
+
+        string curLocationCode = "";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            int m = 0;
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                if ((bool)dataGridView1.Rows[i].Cells[0].EditedFormattedValue == true)
+                {
+                    m++;
+                }
+            }
+            if (m == 0)
+            {
+                MessageBox.Show("请选择要打印的货位");
+                return;
+            }
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                if ((bool)dataGridView1.Rows[i].Cells[0].EditedFormattedValue == true)
+                {
+                    curLocationCode = this.dataGridView1.Rows[i].Cells["locationCode"].Value.ToString();
+
+                    PrintDocument document = new PrintDocument();
+                    document.DefaultPageSettings.PaperSize = new PaperSize("Custum", 500, 400);
+
+#if(!DEBUG)
+                                PrintDialog dialog = new PrintDialog();
+                                document.PrintPage += new PrintPageEventHandler(this.pd_PrintBoxPage);
+                                dialog.Document = document;
+#else
+                    PrintPreviewDialog dialog = new PrintPreviewDialog();
+                    document.PrintPage += new PrintPageEventHandler(this.pd_PrintBoxPage);
+                    dialog.Document = document;
+#endif
+
+                    try
+                    {
+                        document.Print();
+                    }
+                    catch (Exception exception)
+                    {
+                        MessageBox.Show("打印异常" + exception);
+                        document.PrintController.OnEndPrint(document, new PrintEventArgs());
+                    }
+                }
+            }
+         
+
+
+        }
+
+        /// <summary>
+        /// 导出
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+
+            List<LocationsModel> locationsList = new List<LocationsModel>();
+
+            //string path = "d:\\高位货架编码.txt";
+            //StreamReader sr = new StreamReader(path, Encoding.Default);
+            //string line;
+            //while ((line = sr.ReadLine()) != null)
+            //{
+            //    if (!string.IsNullOrWhiteSpace(line))
+            //   {
+            //       LocationsModel locationsModel = new LocationsModel();
+            //       string areaCode="";
+            //       string[] arr = line.Split('-');
+            //       if (line.IndexOf("AB") > 0)
+            //       {
+            //           areaCode = "AB";
+            //       }
+            //       else
+            //       {
+            //           if (line.IndexOf("A") > 0)
+            //           {
+            //               areaCode = "A";
+            //           }
+            //           else
+            //           {
+            //               areaCode = "B";
+            //           }
+            //       }
+            //        Console.WriteLine(line);
+            //        locationsModel.areaCode = areaCode;
+            //        locationsModel.locationCode = line.Trim();
+            //        locationsModel.locationName = line.Trim();
+            //        locationsList.Add(locationsModel);
+            //   }
+            //}
+
+
+            try
+            {
+
+                btnExport.Enabled = false;
+
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "库位(*.xls)|*.xls";
+                sfd.Title = "导出包裹信息";
+                sfd.FilterIndex = 2;
+                sfd.RestoreDirectory = true;
+
+                sfd.FileName = "库位" + ".xls";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+
+                    string areaCode = cbArea.SelectedValue.ToString();
+
+
+                    LocationsQueryRequest request = new LocationsQueryRequest();
+                    request.warehouseCode = UserInfo.WareHouseCode;
+                    request.areaCode = areaCode;
+                    request.locationName = tbName.Text.Trim();
+                    request.page = paginator.PageNo;
+                    request.pageSize = 10000;
+
+                    LocationsResponse response = client.Execute(request);
+
+                    if (!response.IsError)
+                    {
+
+                        if (response.result == null)
+                        {
+                            MessageBox.Show("没有数据!");
+                            return;
+                        }
+                        locationsList = response.result;
+
+                        DataTable dtExecl = new DataTable();
+                        DataColumn dc1 = new DataColumn("      库区编码       ");
+                        dtExecl.Columns.Add(dc1);
+
+                        DataColumn dc2 = new DataColumn("      库位编码     ");
+                        dtExecl.Columns.Add(dc2);
+
+                        DataColumn dc4 = new DataColumn("      库位名称       ");
+                        dtExecl.Columns.Add(dc4);
+
+                        for (int i = 0; i < locationsList.Count; i++)
+                        {
+                            DataRow dr = dtExecl.NewRow();
+                            dr[0] = locationsList[i].areaCode;
+                            dr[1] = locationsList[i].locationCode;
+                            dr[2] = locationsList[i].locationName;
+                            dtExecl.Rows.Add(dr);
+                        }
+
+                        NPOIHelper.ExportDTtoExcel(dtExecl, sfd.Title, "", sfd.FileName);
+
+                        MessageBox.Show("导出成功");
+
+                    }
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("导出异常" + ex.Message);
+            }
+            finally
+            {
+
+                btnExport.Enabled = true;
+            }
+
+        }
+    }
+}
